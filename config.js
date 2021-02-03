61ef6e346f985444f6ffdaf0fc58e6a004b78538
imad <imadatyatalah@gmail.com>
2021-02-03 20:57:32 +0100

    add links

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 export const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export const BASE_URL = "https://dev.to/api/";
+
+export const links = [
+  { title: "Home", href: "/" },
+  { title: "Reading List", href: "/reading-list" },
+  { title: "Listing", href: "/listing" },
+  { title: "Podcasts", href: "/podcasts" },
+  { title: "Videos", href: "/videos" },
+  { title: "Tags", href: "/tags" },
+];
