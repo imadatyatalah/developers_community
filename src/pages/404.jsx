e247ee778f6e735bdee0a659b8f65a73cd81b447
imad <imadatyatalah@gmail.com>
2021-02-03 20:57:54 +0100

    add custom 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,16 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const Custom404 = () => (
+  <>
+    <Box
+      minH="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Heading as="h1">404 - Page Not Found</Heading>
+    </Box>
+  </>
+);
+
+export default Custom404;
