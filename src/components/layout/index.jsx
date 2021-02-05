044ba699c74a32e4fba96fc8eb8ca670d21a4ba9
imad <imadatyatalah@gmail.com>
2021-02-05 20:34:26 +0100

    layout

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@chakra-ui/react";
+import NextHead from "next/head";
+
+const Layout = ({ children }) => (
+  <>
+    <NextHead>
+      <link rel="shortcut icon" href="/dev-ecosystem.png" type="image/x-icon" />
+
+      <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </NextHead>
+
+    <Box as="main" minH="100vh">
+      {children}
+    </Box>
+  </>
+);
+
+export default Layout;
