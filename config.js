88e6fdf7cf5f606644cfe6072a209175caea8e03
imad <imadatyatalah@gmail.com>
2021-02-05 20:29:51 +0100

    add default config

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
+export default {
+  title: "Developers Community",
+  author: {
+    name: "Imad Atyat-Alah",
+  },
+  description:
+    "Developers Community uses dev.to api endpoint to fetch data from, You can call i dev.to clone!",
+};
+
 export const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export const BASE_URL = "https://dev.to/api/";
