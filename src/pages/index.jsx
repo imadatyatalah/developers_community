0fecec354d512e5b4fb45c28deb6d4b3b1f12863
imad <imadatyatalah@gmail.com>
2021-02-06 15:49:25 +0100

    fetch listing endpoint

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,23 +6,32 @@ const Article = dynamic(() => import("../components/article"));
 import config, { fetcher, BASE_URL } from "../../config";
 import SEO from "../components/seo";
 
-const Home = ({ articles }) => {
-  const { data } = useSwr(`${BASE_URL}articles`, fetcher, {
+const Home = ({ articles, listings }) => {
+  const { data: articlesData } = useSwr(`${BASE_URL}articles`, fetcher, {
     initialData: articles,
   });
+
+  const { data: listingsData } = useSwr(
+    `${BASE_URL}listings?per_page=5`,
+    fetcher,
+    {
+      initialData: listings,
+    }
+  );
 
   return (
     <>
       <SEO title="Home" description={config.description} />
-      <Article data={data} />
+      <Article data={articlesData} />
     </>
   );
 };
 
 export const getServerSideProps = async () => {
   const articles = await fetcher(`${BASE_URL}articles`);
+  const listings = await fetcher(`${BASE_URL}listings?per_page=5`);
 
-  return { props: { articles } };
+  return { props: { articles, listings } };
 };
 
 export default Home;
