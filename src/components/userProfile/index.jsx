146b87ae27fa622ccbac38a89ada5e057a0058c4
imad <imadatyatalah@gmail.com>
2021-02-11 21:35:40 +0100

    add tag_line prop

--- src/components/userProfile/index.jsx
+++ src/components/userProfile/index.jsx
@@ -7,7 +7,7 @@ import UserInfo from "./userInfo";
 import styles from "./css/index.module.css";
 
 const UserProfile = (props) => {
-  const { profile_image, name, summary } = props.data;
+  const { profile_image, name, summary, tag_line } = props.data;
 
   return (
     <>
@@ -25,11 +25,13 @@ const UserProfile = (props) => {
             width="100"
             height="100"
             alt={name}
+            priority
             className={styles.userImage}
           />
         </Box>
 
         <Heading as="h1">{name}</Heading>
+        {props.isOrganization && tag_line && <Text py="2">{tag_line}</Text>}
         <Text py="2">{summary ? summary : "404 bio not found"}</Text>
 
         <Box
