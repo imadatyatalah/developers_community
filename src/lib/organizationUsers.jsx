ddd494a9dc51b2eeac98b3c910e5b433dbc7b0ee
imad <imadatyatalah@gmail.com>
2021-02-12 15:12:30 +0100

    fetch organization users

---
+++ src/lib/organizationUsers.jsx
@@ -0,0 +1,9 @@
+import { fetcher, BASE_URL } from "../../config";
+
+export const getOrganizationUsers = async (userName) => {
+  const organizationUsers = await fetcher(
+    `${BASE_URL}organizations/${userName}/users`
+  );
+
+  return organizationUsers;
+};
