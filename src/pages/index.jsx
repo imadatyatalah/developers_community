db48c11ce9540163855c20bc9d36fd36d7c491e9
imad <imadatyatalah@gmail.com>
2021-02-12 18:18:13 +0100

    test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,31 @@
 import dynamic from "next/dynamic";
+import useSwr from "swr";
 
 const HomePage = dynamic(() => import("../components/pages"));
 
-import config from "../../config";
+import config, { BASE_URL, fetcher } from "../../config";
 import { getArticles } from "../lib/articles";
 import { getListings } from "../lib/listings";
 import SEO from "../components/seo";
 
 const Home = ({ articles, listings }) => {
+  const { data: articlesData } = useSwr(`${BASE_URL}articles`, fetcher, {
+    initialData: articles,
+  });
+
+  const { data: listingsData } = useSwr(
+    `${BASE_URL}listings?per_page=5`,
+    fetcher,
+    {
+      initialData: listings,
+    }
+  );
+
   return (
     <>
       <SEO title="Home" description={config.description} />
 
-      <HomePage articles={articles} listings={listings} />
+      <HomePage articles={articlesData} listings={listingsData} />
     </>
   );
 };
