8c36ecdd8263d142cff18f10d603023b4c877bce
imad <imadatyatalah@gmail.com>
2021-02-13 18:00:41 +0100

    remove dynamic import

--- src/pages/[user]/index.jsx
+++ src/pages/[user]/index.jsx
@@ -1,7 +1,4 @@
 import { useRouter } from "next/router";
-import dynamic from "next/dynamic";
-
-const UserPage = dynamic(() => import("../../components/pages/user"));
 
 import { getUser } from "../../lib/user";
 import { getOrganization } from "../../lib/organization";
@@ -9,6 +6,7 @@ import { getUserArticles } from "../../lib/userArticles";
 import { getOrganizationArticles } from "../../lib/organizationArticles";
 import { getOrganizationUsers } from "../../lib/organizationUsers";
 import SEO from "../../components/seo";
+import UserPage from "../../components/pages/user";
 
 const User = ({ userInfo, userArticles, organizationUsers, errorCode }) => {
   const router = useRouter();
