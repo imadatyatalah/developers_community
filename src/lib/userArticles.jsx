9f363c7577ad52ad1d2417d1e790e1703cf5259e
imad <imadatyatalah@gmail.com>
2021-02-14 18:19:08 +0100

    add per_page query

--- src/lib/userArticles.jsx
+++ src/lib/userArticles.jsx
@@ -1,8 +1,8 @@
 import { fetcher, BASE_URL } from "../../config";
 
-export const getUserArticles = async (userName) => {
+export const getUserArticles = async (userName, perPage) => {
   const userArticles = await fetcher(
-    `${BASE_URL}articles?username=${userName}`
+    `${BASE_URL}articles?username=${userName}&per_page=${perPage}`
   );
 
   return userArticles;
