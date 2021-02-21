8151b72a1cc8fb6eaa79f508338145769609a056
imad <imadatyatalah@gmail.com>
2021-02-21 12:22:57 +0100

    fix

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -2,31 +2,39 @@ import { Box, chakra, Heading, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 const Error = ({ statusCode, title }) => (
-  <Box
-    as="section"
-    d="flex"
-    justifyContent="center"
-    alignItems="center"
-    flexDir="column"
-    textAlign="center"
-    minH="100vh"
-    p="5"
-  >
-    <Box py="4">
-      <Heading>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : "An error occurred on client"}
-      </Heading>
-      <Text>{title}</Text>
-    </Box>
+  <>
+    <Box
+      as="section"
+      d="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDir="column"
+      textAlign="center"
+      minH="100vh"
+      p="5"
+    >
+      <Box py="4">
+        <Heading as="h1">
+          {statusCode
+            ? `An error ${statusCode} occurred on server`
+            : "An error occurred on client"}
+        </Heading>
+        <Text fontSize="lg">{title}</Text>
+      </Box>
 
-    <NextLink href="/">
-      <chakra.a href="/" fontSize="xl" fontWeight="700">
-        Back to home page
-      </chakra.a>
-    </NextLink>
-  </Box>
+      <NextLink href="/">
+        <chakra.a
+          href="/"
+          fontSize={{ base: "xl", sm: "2xl" }}
+          fontWeight="700"
+          transitionDuration="250ms"
+          _hover={{ color: "teal.400" }}
+        >
+          Back to home page
+        </chakra.a>
+      </NextLink>
+    </Box>
+  </>
 );
 
 Error.getInitialProps = ({ res, err }) => {
