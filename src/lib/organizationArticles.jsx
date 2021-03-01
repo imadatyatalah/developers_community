a0e998e6530584952efbf08f0117247598e6c8ce
imad <imadatyatalah@gmail.com>
2021-03-01 16:33:09 +0100

    Delete unused files

--- src/lib/organizationArticles.jsx
+++
@@ -1,9 +0,0 @@
-import { fetcher, BASE_URL } from "../../config";
-
-export const getOrganizationArticles = async (userName) => {
-  const organizationArticles = await fetcher(
-    `${BASE_URL}organizations/${userName}/articles`
-  );
-
-  return organizationArticles;
-};
