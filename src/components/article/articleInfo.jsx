867effd6892aedb4c3f7c623430454ea9eb5c048
imad <imadatyatalah@gmail.com>
2021-03-01 14:41:34 +0100

    Fix

--- src/components/article/articleInfo.jsx
+++ src/components/article/articleInfo.jsx
@@ -1,22 +1,81 @@
 import { Box, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
+import NextImage from "next/image";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-
-import ProfileImg from "./profileImg";
-import UserName from "./userName";
 
 dayjs.extend(relativeTime);
 
 const ArticleInfo = (props) => {
   const { data } = props;
 
+  if (data.organization && props.isUser) {
+    return (
+      <>
+        <Box as="header" display="flex" alignItems="center">
+          <NextLink href={`/${data.organization.username}`}>
+            <a className="articleOrgImage">
+              <NextImage
+                src={data.organization.profile_image_90}
+                width="40"
+                height="40"
+                alt={data.organization.name}
+                className="orgImage"
+              />
+            </a>
+          </NextLink>
+
+          <NextLink href={`/${data.user.username}`}>
+            <a className="orgArticleUserImage">
+              <NextImage
+                src={data.user.profile_image_90}
+                width="30"
+                height="30"
+                alt={data.user.name}
+                className="userImage"
+              />
+            </a>
+          </NextLink>
+
+          <Box>
+            <NextLink href={`/${data.user.username}`}>
+              <a className="articleUserName">{data.user.name}</a>
+            </NextLink>{" "}
+            <Text as="span" color="gray.600">
+              for{" "}
+            </Text>
+            <NextLink href={`/${data.organization.username}`}>
+              <a className="articleUserName">{data.organization.name}</a>
+            </NextLink>
+            <Text fontSize="sm" fontWeight="300">
+              {data.readable_publish_date} ({dayjs(data.published_at).fromNow()}
+              )
+            </Text>
+          </Box>
+        </Box>
+      </>
+    );
+  }
+
   return (
     <>
       <Box as="header" display="flex" alignItems="center">
-        <ProfileImg {...props} />
+        <NextLink href={`/${data.user.username}`}>
+          <a className="articleUserImage">
+            <NextImage
+              src={data.user.profile_image_90}
+              width="40"
+              height="40"
+              alt={data.user.name}
+              className="userImage"
+            />
+          </a>
+        </NextLink>
 
         <Box>
-          <UserName {...props} />
+          <NextLink href={`/${data.user.username}`}>
+            <a className="articleUserName">{data.user.name}</a>
+          </NextLink>
 
           <Text fontSize="sm" fontWeight="300">
             {data.readable_publish_date} ({dayjs(data.published_at).fromNow()})
