28fcce5d7c9971fae84c0bc813c8515c8952668e
imad <imadatyatalah@gmail.com>
2021-03-18 16:18:24 +0100

    Using react-query with Using Hydration

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,3 +1,7 @@
+import { useRef } from "react"
+import { Hydrate } from "react-query/hydration"
+import { ReactQueryDevtools } from "react-query/devtools"
+import { QueryClientProvider, QueryClient } from "react-query"
 import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 import { DefaultSeo } from "next-seo"
 
@@ -18,16 +22,29 @@ const theme = extendTheme({
   },
 })
 
-const MyApp = ({ Component, pageProps }) => (
-  <>
-    <DefaultSeo {...SEO} />
+const MyApp = ({ Component, pageProps }) => {
+  const queryClientRef = useRef()
 
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
-  </>
-)
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient()
+  }
+
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+
+      <QueryClientProvider client={queryClientRef.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ChakraProvider theme={theme}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ChakraProvider>
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
+  )
+}
 
 export default MyApp
